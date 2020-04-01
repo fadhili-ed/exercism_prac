2c6757b303dd8abf8049ca82973798525c7d3c59
fadhili-ed <fadhilied00@gmail.com>
2020-04-01 11:44:34 +0300

    first commit

---
+++ reverse-string.js
@@ -0,0 +1,12 @@
+//
+// This is only a SKELETON file for the 'Reverse String' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export const reverseString = (word) => {
+  if(word == '') {
+    return ''
+  }else {
+    word.split('').reverse().join('')
+  }
+};
