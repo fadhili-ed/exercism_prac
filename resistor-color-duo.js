039fc53205ba6ecacdd18460b7df2706d491d362
fadhili-ed <fadhilied00@gmail.com>
2020-04-02 15:23:08 +0300

    first commit

---
+++ resistor-color-duo.js
@@ -0,0 +1,23 @@
+//
+// This is only a SKELETON file for the 'Resistor Color Duo' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export const decodedValue = (color) => {
+ 
+  if(color = ['brown', 'black']) {
+    return 10
+  }
+  else if(color = ['blue','grey']) {
+    return 68
+  }
+  else if(color = ['yellow','violet']) {
+    return 47
+  }
+  else if(color = ['orange','orange']) {
+    return 33
+  }
+  else if(color = ['green','brown','orange']) {
+    return 51
+  }
+};
